90aaa8824c4b0e8f2d3a7b83d4bedc98147500a5
Ahmed933 <ahmedalaaelaref@gmail.com>
2020-01-13 04:20:24 +0200

    Update DIO_cfg.h

--- MCAL/DIO/DIO_cfg.h
+++ MCAL/DIO/DIO_cfg.h
@@ -1,47 +1,51 @@
-/*******************************/
-/** Author      : Ahmed Alaa       */
-/** Description : DIO configration */
-/** Date        : 22 NOV 19        */
-/** Version     : 1.0V             */
-/*******************************/
+/**
+ ** Author      : Ahmed El-Aref    
+ ** Description : DIO Configuration
+ ** Date        : 22 NOV 19     
+ ** Version     : 1.0V          
+ **/
 
 #ifndef DIO_CFG_H
 #define DIO_CFG_H
 
-#define u8PINDIR_0 		OUTPUT
-#define u8PINDIR_1      OUTPUT 
-#define u8PINDIR_2 		OUTPUT
-#define u8PINDIR_3 		OUTPUT	
-#define u8PINDIR_4      OUTPUT
-#define u8PINDIR_5 		OUTPUT
-#define u8PINDIR_6 		OUTPUT
-#define u8PINDIR_7 		OUTPUT
+/** PORTA PINS **/
+#define PORTA_B0 		OUTPUT
+#define PORTA_B1        OUTPUT 
+#define PORTA_B2 		OUTPUT
+#define PORTA_B3 		OUTPUT	
+#define PORTA_B4        OUTPUT
+#define PORTA_B5 		OUTPUT
+#define PORTA_B6 		OUTPUT
+#define PORTA_B7 		OUTPUT
 
-#define u8PINDIR_8 		OUTPUT
-#define u8PINDIR_9 		OUTPUT
-#define u8PINDIR_10		OUTPUT
-#define u8PINDIR_11		OUTPUT
-#define u8PINDIR_12     OUTPUT
-#define u8PINDIR_13     OUTPUT
-#define u8PINDIR_14     OUTPUT
-#define u8PINDIR_15     OUTPUT
+/** PORTB PINS **/
+#define PORTB_B0		OUTPUT
+#define PORTB_B1		OUTPUT
+#define PORTB_B2		OUTPUT
+#define PORTB_B3		OUTPUT
+#define PORTB_B4     	OUTPUT
+#define PORTB_B5     	OUTPUT
+#define PORTB_B6     	OUTPUT
+#define PORTB_B7     	OUTPUT
 
-#define u8PINDIR_16		OUTPUT
-#define u8PINDIR_17     OUTPUT
-#define u8PINDIR_18     OUTPUT
-#define u8PINDIR_19     OUTPUT
-#define u8PINDIR_20     OUTPUT
-#define u8PINDIR_21     OUTPUT
-#define u8PINDIR_22     OUTPUT
-#define u8PINDIR_23     OUTPUT
+/** PORTC PINS **/
+#define PORTC_B0		OUTPUT
+#define PORTC_B1        OUTPUT
+#define PORTC_B2        OUTPUT
+#define PORTC_B3        OUTPUT
+#define PORTC_B4        OUTPUT
+#define PORTC_B5        OUTPUT
+#define PORTC_B6        OUTPUT
+#define PORTC_B7        OUTPUT
 
-#define u8PINDIR_24		OUTPUT
-#define u8PINDIR_25     OUTPUT
-#define u8PINDIR_26     OUTPUT
-#define u8PINDIR_27     OUTPUT
-#define u8PINDIR_28     OUTPUT
-#define u8PINDIR_29     OUTPUT
-#define u8PINDIR_30     OUTPUT
-#define u8PINDIR_31     OUTPUT
+/** PORTD PINS **/
+#define PORTD_B0		OUTPUT
+#define PORTD_B1        OUTPUT
+#define PORTD_B2        OUTPUT
+#define PORTD_B3        OUTPUT
+#define PORTD_B4        OUTPUT
+#define PORTD_B5        OUTPUT
+#define PORTD_B6        OUTPUT
+#define PORTD_B7        OUTPUT
 
 #endif           
