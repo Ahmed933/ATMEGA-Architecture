fa29c4b8351b095e93cbf302042591bcf32b0114
Ahmed933 <ahmedalaaelaref@gmail.com>
2020-01-13 07:35:48 +0200

    updated

--- MCAL/DIO/DIO_cfg.h
+++ MCAL/DIO/DIO_cfg.h
@@ -8,44 +8,44 @@
 #ifndef DIO_CFG_H
 #define DIO_CFG_H
 
-/** PORTA PINS **/
-#define PORTA_B0 		OUTPUT
-#define PORTA_B1        OUTPUT 
-#define PORTA_B2 		OUTPUT
-#define PORTA_B3 		OUTPUT	
-#define PORTA_B4        OUTPUT
-#define PORTA_B5 		OUTPUT
-#define PORTA_B6 		OUTPUT
-#define PORTA_B7 		OUTPUT
+/** PORTA PINS DIRECTION **/
+#define PORTA_B0_DIR 		OUTPUT
+#define PORTA_B1_DIR        OUTPUT
+#define PORTA_B2_DIR 		OUTPUT
+#define PORTA_B3_DIR 		OUTPUT
+#define PORTA_B4_DIR        OUTPUT
+#define PORTA_B5_DIR 		OUTPUT
+#define PORTA_B6_DIR 		OUTPUT
+#define PORTA_B7_DIR 		OUTPUT
 
-/** PORTB PINS **/
-#define PORTB_B0		OUTPUT
-#define PORTB_B1		OUTPUT
-#define PORTB_B2		OUTPUT
-#define PORTB_B3		OUTPUT
-#define PORTB_B4     	OUTPUT
-#define PORTB_B5     	OUTPUT
-#define PORTB_B6     	OUTPUT
-#define PORTB_B7     	OUTPUT
+/** PORTB PINS DIRECTION **/
+#define PORTB_B0_DIR		OUTPUT
+#define PORTB_B1_DIR		OUTPUT
+#define PORTB_B2_DIR		OUTPUT
+#define PORTB_B3_DIR		OUTPUT
+#define PORTB_B4_DIR     	OUTPUT
+#define PORTB_B5_DIR     	OUTPUT
+#define PORTB_B6_DIR     	OUTPUT
+#define PORTB_B7_DIR     	OUTPUT
 
-/** PORTC PINS **/
-#define PORTC_B0		OUTPUT
-#define PORTC_B1        OUTPUT
-#define PORTC_B2        OUTPUT
-#define PORTC_B3        OUTPUT
-#define PORTC_B4        OUTPUT
-#define PORTC_B5        OUTPUT
-#define PORTC_B6        OUTPUT
-#define PORTC_B7        OUTPUT
+/** PORTC PINS DIRECTION **/
+#define PORTC_B0_DIR		INPUT
+#define PORTC_B1_DIR        INPUT
+#define PORTC_B2_DIR        OUTPUT
+#define PORTC_B3_DIR        OUTPUT
+#define PORTC_B4_DIR        OUTPUT
+#define PORTC_B5_DIR        OUTPUT
+#define PORTC_B6_DIR        OUTPUT
+#define PORTC_B7_DIR        OUTPUT
 
-/** PORTD PINS **/
-#define PORTD_B0		OUTPUT
-#define PORTD_B1        OUTPUT
-#define PORTD_B2        OUTPUT
-#define PORTD_B3        OUTPUT
-#define PORTD_B4        OUTPUT
-#define PORTD_B5        OUTPUT
-#define PORTD_B6        OUTPUT
-#define PORTD_B7        OUTPUT
+/** PORTD PINS DIRECTION **/
+#define PORTD_B0_DIR		OUTPUT
+#define PORTD_B1_DIR        OUTPUT
+#define PORTD_B2_DIR        OUTPUT
+#define PORTD_B3_DIR        OUTPUT
+#define PORTD_B4_DIR        OUTPUT
+#define PORTD_B5_DIR        OUTPUT
+#define PORTD_B6_DIR        OUTPUT
+#define PORTD_B7_DIR        OUTPUT
 
 #endif           
